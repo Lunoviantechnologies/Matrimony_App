d16c8a106770983725f934a8e8a2c9d828812604
AkshayDuggirala <2100031105cseh@gmail.com>
2026-05-04 11:29:17 +0530

    enable 120Hz high refresh rate display mode for Android

--- android/app/src/main/java/com/matrimonyapp/MainActivity.kt
+++ android/app/src/main/java/com/matrimonyapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.matrimonyapp
-
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +21,57 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * Override onCreate to set high refresh rate from app start
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableMaxRefreshRate()
+  }
+
+  /**
+   * Override onResume to aggressively force the high refresh rate every time
+   * the app comes to the foreground, bypassing React Native's default 60Hz cap.
+   */
+  override fun onResume() {
+      super.onResume()
+      enableMaxRefreshRate()
+  }
+
+  /**
+   * Enable maximum refresh rate using the proven preferredDisplayModeId method
+   */
+  private fun enableMaxRefreshRate() {
+      try {
+          // Android 6.0+ (API 23+) - Set preferred display mode ID
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+              try {
+                  val display = windowManager.defaultDisplay
+                  val supportedModes = display.supportedModes
+                  
+                  if (supportedModes.isNotEmpty()) {
+                      // Find the display mode with the highest refresh rate
+                      var highestRefreshRateMode = supportedModes[0]
+                      
+                      for (mode in supportedModes) {
+                          if (mode.refreshRate > highestRefreshRateMode.refreshRate) {
+                              highestRefreshRateMode = mode
+                          }
+                      }
+                      
+                      // Apply the highest refresh rate mode
+                      val layoutParams = window.attributes
+                      layoutParams.preferredDisplayModeId = highestRefreshRateMode.modeId
+                      window.attributes = layoutParams
+                  }
+              } catch (e: Exception) {
+                  e.printStackTrace()
+              }
+          }
+      } catch (e: Exception) {
+          // Failsafe so the app doesn't crash
+          e.printStackTrace()
+      }
+  }
 }
